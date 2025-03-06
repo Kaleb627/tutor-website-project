7ae6286654154046d2069abe3e936be25342ab29
kaleb-work <kaleb6277@gmail.com>
2025-03-06 12:41:39 -0500

    Followed walkthrough for navigation section and refactored / commented code to reflect course

--- tutor-website/js/script.js
+++ tutor-website/js/script.js
@@ -1,3 +1,12 @@
-// Toggle mobile menu
+// we want to listen for the event of the page loading then run the following function
+document.addEventListener('DOMContentLoaded', function () {
+    // then we need to select the hamburger menu button and the menu
+    const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
+    const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
 
-// Change navbar background on scroll
+    // then we want to listen for when the toggle button is clicked and add/remove the active class to  .mobile-menu-items so that the menu will sweep in and out
+    toggleButton.addEventListener('click', function() {
+        mobileMenu.classList.toggle('active')
+    })
+
+})
