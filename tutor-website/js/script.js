d917350f4a50f8c4f83a0919d613574b8a19db65
kaleb-work <kaleb6277@gmail.com>
2025-03-07 17:54:16 -0500

    first draft of navbar transperency and blur on scroll

--- tutor-website/js/script.js
+++ tutor-website/js/script.js
@@ -9,4 +9,21 @@ document.addEventListener('DOMContentLoaded', function () {
     toggleButton.addEventListener('click', function() {
         mobileMenu.classList.toggle('active')
     })
+
+    // function to give navbar a semi trasnparent background when scrolled past default position to make it more readable as you move down the page
+})
+
+// make navigation bar semi transparent as you scroll to aid readability via javascript to add a class based on scroll position
+// if we use the window object, this references the browser window and looking at the window we can use an event listener to see the scroll poisition
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+
+    // console.log(this.window.scrollY);
+    if (window.scrollY > 0) {
+        // add the class ".navbar-scroll" using classList.add 
+        navbar.classList.add('.navbar-scroll');
+    }
+    else {
+        navbar.classList.remove('.navbar-scroll');
+    }
 })
